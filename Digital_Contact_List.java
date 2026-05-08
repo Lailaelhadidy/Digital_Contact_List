c473f29251e16052b50632676f3ad0a964ba4107
Laila Elhadidy <lailahassanelhadidy@gmail.com>
2026-05-09 01:23:11 +0300

    Update Digital_Contact_List.java

--- Digital_Contact_List.java
+++ Digital_Contact_List.java
@@ -140,97 +140,114 @@ public class Digital_Contact_List {
             
            } while(option != 0);
 
-//      ArrayPhoneBook phonebook = new ArrayPhoneBook();
-//        
-
-//        
+//ArrayPhoneBook phonebook = new ArrayPhoneBook();
+//
 //        do {
 //            System.out.print("""
-//                           
-//                           Choose an option:
-//                           1. Add contact
-//                           2. Remove contact
-//                           3. Update contact phone
-//                           4. Update Contact Name
-//                           5. Search contact
-//                           6. Display all contacts
-//                           7. Size of contacts
-//                           8. Display isolated contacts (0100)
-//                           0. Exit
-//                           Enter choice: 
-//                           """);
-//            
-//            option = in.nextInt();
-//            in.nextLine(); // very important after nextInt()
-//            
-//            switch(option) {
-//                case 1: {
-//                    System.out.println("Enter Person Name: ");
-//                    String name = in.nextLine();
-//                    
-//                    System.out.println("Enter Person Phone: ");
-//                    String phone = in.nextLine();
-//                    
-//                    Person p = new Person(name, phone);
-//                    phonebook.insert(p);
-//                    break;
+//                               
+//                               Choose an option:
+//                               1. Add contact
+//                               2. Remove contact
+//                               3. Update contact phone
+//                               4. Update contact name
+//                               5. Search contact
+//                               6. Display all contacts
+//                               7. Size of contacts
+//                               8. Display isolated contacts (0100)
+//                               0. Exit
+//                               Enter choice:
+//                               """);
+//
+//            try {
+//                option = in.nextInt();
+//                in.nextLine();
+//
+//                switch (option) {
+//
+//                    case 1: {
+//                        System.out.println("Enter Person Name:");
+//                        String name = in.nextLine();
+//
+//                        System.out.println("Enter Person Phone:");
+//                        String phone = in.nextLine();
+//
+//                        phonebook.insert(new Person(name, phone));
+//                        break;
+//                    }
+//
+//                    case 2: {
+//                        System.out.println("Enter Person Name:");
+//                        String name = in.nextLine();
+//
+//                        phonebook.remove(name);
+//                        break;
+//                    }
+//
+//                    case 3: {
+//                        System.out.println("Enter Contact Name:");
+//                        String name = in.nextLine();
+//
+//                        Person result = phonebook.search(name);
+//
+//                        if (result != null) {
+//                            System.out.println("Enter New Phone:");
+//                            String phone = in.nextLine();
+//
+//                            phonebook.updateExistingPhone(name, phone);
+//                        }
+//
+//                        break;
+//                    }
+//
+//                    case 4: {
+//                        System.out.println("Enter Current Contact Name:");
+//                        String oldName = in.nextLine();
+//
+//                        Person result = phonebook.search(oldName);
+//
+//                        if (result != null) {
+//                            System.out.println("Enter New Contact Name:");
+//                            String newName = in.nextLine();
+//
+//                            phonebook.updateExistingName(oldName, newName);
+//                        }
+//
+//                        break;
+//                    }
+//
+//                    case 5: {
+//                        System.out.println("Enter Person Name:");
+//                        String name = in.nextLine();
+//
+//                        phonebook.search(name);
+//                        break;
+//                    }
+//
+//                    case 6:
+//                        phonebook.display();
+//                        break;
+//
+//                    case 7:
+//                        phonebook.size();
+//                        break;
+//
+//                    case 8:
+//                        phonebook.isolate0100();
+//                        break;
+//
+//                    case 0:
+//                        System.out.println("Exiting Program... Goodbye!");
+//                        break;
+//
+//                    default:
+//                        System.out.println("Invalid Choice.. Try again");
 //                }
-//                
-//                case 2: {
-//                    System.out.println("Enter Person Name: ");
-//                    String name = in.nextLine();
-//                    phonebook.remove(name);
-//                    break;
-//                }
-//                
-//                case 3: {
-//                    System.out.println("Enter Contact Name: ");
-//                    String name = in.nextLine();
-//                    
-//                    System.out.println("Enter new Phone: ");
-//                    String phone = in.nextLine();
-//                    
-//                    phonebook.updateExistingPhone(name, phone);
-//                    break;
-//                }
-//                case 4: {
-//                    System.out.println("Enter Contact Name: ");
-//                    String name = in.nextLine();
-//                    
-//                    System.out.println("Enter new Name: ");
-//                    String phone = in.nextLine();
-//                    
-//                    phonebook.updateExistingName(name, phone);
-//                    break;
-//                }
-//                
-//                case 5: {
-//                    System.out.println("Enter Person Name: ");
-//                    String name = in.nextLine();
-//                    phonebook.search(name);
-//                    break;
-//                }
-//                
-//                case 6:
-//                    phonebook.display();
-//                    break;
-//                    
-//                case 7:
-//                    phonebook.size();
-//                    break;
-//                    
-//                case 8:
-//                    phonebook.isolate0100();
-//                    break;
-//                    
-//                case 0:
-//                    System.out.println("Exiting Program... Goodbye!");
-//                    break;
-//                    
-//                default:
-//                    System.out.println("Invalid Choice.. Try again");
+//
+//            } catch (Exception e) {
+//                System.out.println("Invalid input! Please enter a number.");
+//                in.nextLine();
 //            }
-//            
-//        } while(option != 0);
+//
+//        } while (option != 0);
     }
 }
